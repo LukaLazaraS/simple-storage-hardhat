d0c9c38263ad8693dc7e57e255df0618693581f9
LukaLazarashvili <Lazarashvili02@gmail.com>
2023-03-09 00:48:26 +0400

    add goerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.18",
 };
 
