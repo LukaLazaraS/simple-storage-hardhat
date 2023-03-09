5b4fd2c89fc4e8970886fc9a502fd254638e3451
LukaLazarashvili <Lazarashvili02@gmail.com>
2023-03-09 04:13:14 +0400

    add auto verify

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,27 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   const SimpleStorage = await SimpleStorageFactory.deploy();
   await SimpleStorage.deployed();
-  console.log(`Deploed to + ${SimpleStorage.address}`);
+  console.log(`Contract deploed to + ${SimpleStorage.address}`);
+
+  if (network.name !== "hardhat" && process.env.ETHERSCAN_API_KEY) {
+    await SimpleStorage.deployTransaction.wait(6);
+    verify(SimpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress: string, args: any[]) {
+  console.log("verifying");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constuctorArguments: args,
+    });
+  } catch (e: any) {
+    console.log(e);
+  }
 }
 
 main().catch((error) => {
